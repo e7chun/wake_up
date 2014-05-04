bdec1d38f0d466d71022e179265ed5c9394295c4
willtsay <willtsay@gmail.com>
2014-05-04 13:36:51 -0700

    alarmclock.js

--- alarmclock.js
+++ alarmclock.js
@@ -1,77 +1,66 @@
-function updateClock ( )
-{
-  var currentTime = new Date ( );
-  var currentHours = currentTime.getHours ( );
-  var currentMinutes = currentTime.getMinutes ( );
-  var currentSeconds = currentTime.getSeconds ( );
-  var alarmTime = "070000"
-  // Pad the minutes and seconds with leading zeros, if required
-  currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
-  currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+// function updateClock ( )
+// {
+//   var currentTime = new Date ( );
+//   var currentHours = currentTime.getHours ( );
+//   var currentMinutes = currentTime.getMinutes ( );
+//   var currentSeconds = currentTime.getSeconds ( );
+//   var alarmTime = "070000"
+//   // Pad the minutes and seconds with leading zeros, if required
+//   currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
+//   currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
 
-  // Choose either "AM" or "PM" as appropriate
-  var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
+//   // Choose either "AM" or "PM" as appropriate
+//   var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
 
-  // Convert the hours component to 12-hour format if needed
-  currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
+//   // Convert the hours component to 12-hour format if needed
+//   currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
 
-  // Convert an hours component of "0" to "12"
-  currentHours = ( currentHours == 0 ) ? 12 : currentHours;
+//   // Convert an hours component of "0" to "12"
+//   currentHours = ( currentHours == 0 ) ? 12 : currentHours;
 
-  // Compose the string for display
-  var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
-  var rawTime = currentHours + currentMinutes + currentSeconds
-  // Update the time display
-  document.getElementById("clock").firstChild.nodeValue = currentTimeString;
+//   // Compose the string for display
+//   var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+//   var rawTime = currentHours + currentMinutes + currentSeconds
+//   // Update the time display
+//   document.getElementById("clock").firstChild.nodeValue = currentTimeString;
 
-}
+// }
   
-// <body onload="updateClock(); setInterval('updateClock()', 1000 )"> runs updates clock every second. 
+// // <body onload="updateClock(); setInterval('updateClock()', 1000 )"> runs updates clock every second. 
 
-// timer starts when alarm starts
-// button appears when alarm starts 
-//checkin to GTFU -> on click says STFU -> 
-window.onload = function() {
-    document.getElementById('check_in').addEventListener('click', function() {
-    document.getElementById('check_in').innerHTML = "<h1>STAY THE FUCK UP</h1>"
-    setInterval(enableDisable, 1000); // checks enableDisable
-    enableDisable()
-    })
-}
+// // timer starts when alarm starts
+// // button appears when alarm starts 
+// //checkin to GTFU -> on click says STFU -> 
+
 
 var enableDisable = function(){
-  var hour = 12
-  var currentHour = 12
-  if ( hour == currentHour){
-    document.getElementById('check_in').disabled = true;
-    document.getElementById('check_in').style.display = 'none';
+  
+  if ( time_left == 0 ){
+    document.getElementById('end').style.visibility = 'visible';
+    document.getElementById('end').addEventListener('click', function() {
+        document.getElementById('end').innerHTML = "gratz!"
+    })
   }
   else
   {
-    document.getElementById("check_in").disabled = true;
-    document.getElementById("check_in").style.display = 'none';  
+    document.getElementById("end").style.visibility = 'hidden';  
   }
 }
-window. onload = function() { 
+
+window.onload = function() {
+    document.getElementById('start').addEventListener('click', function() {
+    document.getElementById('start').innerHTML = "STAY THE F*CK UP"
+    setInterval(im_still_awake, 1000)
+    im_still_awake()
+    })
+    setInterval(enableDisable, 1000); // checks enableDisable
+    enableDisable()
+}
+  
+var time_left = 10
+var im_still_awake = function(){
+  if (time_left > 0) 
+  {
+    time_left--;
   }
-// enable disables a button or somethn  i could do some inline crap to make it do something else.. i guess?
-
-var time_elapsed = 0
-var checkinTimer = function(time_elapsed){
-  time_elapsed++;
-  console.log(time_elapsed)
 }
-var stop_timer = function(){
-  document.getElementById('check_in').addEventListener('click')
-}
-
-var time = 0
-var updateTimer = function(){
-
-  document.getElementById("count_up").firstChild.nodeValue = time;
-  time++;
-}
-
-
-
-
